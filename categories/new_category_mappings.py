433f40854867ad97712d5e01f6193400756a473e
anselbrandt <ansel@anselbrandt.com>
2025-05-19 15:58:03 -0400

    Update categories

--- categories/new_category_mappings.py
+++ categories/new_category_mappings.py
@@ -1,4 +1,26 @@
 NEW_CATEGORY_MAPPINGS = {
+    "latest": {
+        "name": "latest",
+        "displayName": "Latest",
+        "subcategories": {
+            "latest": {
+                "name": "latest",
+                "displayName": "Latest",
+                "match_categories": [
+                    ["arts", "Books"],
+                    ["education", "Education"],
+                    ["news", "Business News"],
+                    ["news", "Daily News"],
+                    ["news", "News Commentary"],
+                    ["news", "News"],
+                    ["news", "Politics"],
+                    ["society_and_culture", "Documentary"],
+                    ["society_and_culture", "Philosophy"],
+                    ["society_and_culture", "Society & Culture"],
+                ],
+            }
+        },
+    },
     "arts": {
         "name": "arts",
         "displayName": "Arts",
@@ -302,6 +324,90 @@ NEW_CATEGORY_MAPPINGS = {
         "name": "news",
         "displayName": "News",
         "subcategories": {
+            "world": {
+                "name": "world",
+                "displayName": "World",
+                "desc": "World news and international affairs",
+                "match_categories": [
+                    ["news", "Business News"],
+                    ["news", "Daily News"],
+                    ["news", "News"],
+                    ["news", "News Commentary"],
+                    ["news", "Politics"],
+                ],
+            },
+            "us": {
+                "name": "us",
+                "displayName": "US",
+                "desc": "News about the United States",
+                "match_categories": [
+                    ["news", "Business News"],
+                    ["news", "Daily News"],
+                    ["news", "News"],
+                    ["news", "News Commentary"],
+                    ["news", "Politics"],
+                ],
+            },
+            "canada": {
+                "name": "canada",
+                "displayName": "Canada",
+                "desc": "News about Canada",
+                "match_categories": [
+                    ["news", "Business News"],
+                    ["news", "Daily News"],
+                    ["news", "News"],
+                    ["news", "News Commentary"],
+                    ["news", "Politics"],
+                ],
+            },
+            "uk": {
+                "name": "uk",
+                "displayName": "UK",
+                "desc": "News about the United Kingdom",
+                "match_categories": [
+                    ["news", "Business News"],
+                    ["news", "Daily News"],
+                    ["news", "News"],
+                    ["news", "News Commentary"],
+                    ["news", "Politics"],
+                ],
+            },
+            "europe": {
+                "name": "europe",
+                "displayName": "Europe",
+                "desc": "News about Europe",
+                "match_categories": [
+                    ["news", "Business News"],
+                    ["news", "Daily News"],
+                    ["news", "News"],
+                    ["news", "News Commentary"],
+                    ["news", "Politics"],
+                ],
+            },
+            "ukraine": {
+                "name": "ukraine",
+                "displayName": "Ukraine",
+                "desc": "News related to Ukraine and the war",
+                "match_categories": [
+                    ["news", "Business News"],
+                    ["news", "Daily News"],
+                    ["news", "News"],
+                    ["news", "News Commentary"],
+                    ["news", "Politics"],
+                ],
+            },
+            "middleeast": {
+                "name": "middleeast",
+                "displayName": "Middle East",
+                "desc": "News from the Middle East region",
+                "match_categories": [
+                    ["news", "Business News"],
+                    ["news", "Daily News"],
+                    ["news", "News"],
+                    ["news", "News Commentary"],
+                    ["news", "Politics"],
+                ],
+            },
             "business_news": {
                 "name": "business_news",
                 "displayName": "Business News",
