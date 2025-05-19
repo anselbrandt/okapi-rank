a18d25887482585ee1b8cad3d7b1a4a464771ef6
anselbrandt <ansel@anselbrandt.com>
2025-05-19 00:30:27 -0400

    Update layout

--- frontend/src/components/FrontPage.tsx
+++ frontend/src/components/FrontPage.tsx
@@ -27,59 +27,61 @@ export const FrontPage = ({
 
   return (
     <main className="w-screen flex flex-col items-center">
-      <div className="w-full lg:max-w-7xl flex flex-row">
-        <div className="w-full lg:max-w-3xl xl:max-w-5xl p-8 bg-neutral-50 text-gray-900 pb-8">
-          {first && (
-            <Hero
-              key={first.embedId}
-              episode={first}
-              expandedSummaries={expandedSummaries}
-              toggleSummary={toggleSummary}
-              currentEmbedUrl={currentEmbedUrl}
-              setCurrentEmbedUrl={setCurrentEmbedUrl}
-            />
-          )}
-        </div>
-        <div className="hidden md:block md:max-w-2xs lg:max-w-sm pt-8 pr-8">
-          <SidebarBlock
-            episodes={episodes}
-            currentEmbedUrl={currentEmbedUrl}
-            setCurrentEmbedUrl={setCurrentEmbedUrl}
-            rowOrColumn="col"
-            sectionTitle="Section 1"
-          />
-        </div>
-      </div>
-      <div className="w-full lg:max-w-7xl flex flex-row">
-        <div className="w-full lg:max-w-3xl xl:max-w-5xl p-8 bg-neutral-50 text-gray-900 space-y-6 pb-8">
-          <div className="space-y-6 lg:grid lg:grid-cols-2">
-            {rest.map((episode) => (
-              <FrontPageItem
-                key={episode.embedId}
-                episode={episode}
+      <div className="w-full lg:max-w-7xl ">
+        <div className="flex flex-row">
+          <div className="w-full lg:max-w-3xl xl:max-w-5xl p-8 bg-neutral-50 text-gray-900 pb-8">
+            {first && (
+              <Hero
+                key={first.embedId}
+                episode={first}
                 expandedSummaries={expandedSummaries}
                 toggleSummary={toggleSummary}
                 currentEmbedUrl={currentEmbedUrl}
                 setCurrentEmbedUrl={setCurrentEmbedUrl}
               />
-            ))}
+            )}
+          </div>
+          <div className="hidden md:block md:max-w-2xs lg:max-w-sm pt-8 pr-8">
+            <SidebarBlock
+              episodes={episodes}
+              currentEmbedUrl={currentEmbedUrl}
+              setCurrentEmbedUrl={setCurrentEmbedUrl}
+              rowOrColumn="col"
+              sectionTitle="Section 1"
+            />
           </div>
         </div>
-        <div className="hidden md:block md:max-w-2xs lg:max-w-sm pt-8 pr-8">
-          <SidebarBlock
-            episodes={episodes}
-            currentEmbedUrl={currentEmbedUrl}
-            setCurrentEmbedUrl={setCurrentEmbedUrl}
-            rowOrColumn="row"
-            sectionTitle="Section 2"
-          />
+        <div className="flex flex-row">
+          <div className="w-full lg:max-w-3xl xl:max-w-5xl p-8 bg-neutral-50 text-gray-900 space-y-6 pb-8">
+            <div className="space-y-6 lg:grid lg:grid-cols-2">
+              {rest.map((episode) => (
+                <FrontPageItem
+                  key={episode.embedId}
+                  episode={episode}
+                  expandedSummaries={expandedSummaries}
+                  toggleSummary={toggleSummary}
+                  currentEmbedUrl={currentEmbedUrl}
+                  setCurrentEmbedUrl={setCurrentEmbedUrl}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="hidden md:block md:max-w-2xs lg:max-w-sm pt-8 pr-8">
+            <SidebarBlock
+              episodes={episodes}
+              currentEmbedUrl={currentEmbedUrl}
+              setCurrentEmbedUrl={setCurrentEmbedUrl}
+              rowOrColumn="row"
+              sectionTitle="Section 2"
+            />
+          </div>
         </div>
-      </div>
-      <div className="w-full lg:max-w-7xl grid grid-cols-2 lg:grid-cols-4 gap-4 p-8 pb-50">
-        <div>Column 1</div>
-        <div>Column 2</div>
-        <div>Column 3</div>
-        <div>Column 4</div>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 p-8 pb-50">
+          <div>Column 1</div>
+          <div>Column 2</div>
+          <div>Column 3</div>
+          <div>Column 4</div>
+        </div>
       </div>
     </main>
   );
